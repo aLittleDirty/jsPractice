9aa4aa0f951c8f477a72049e73f4d3c3e5685d59
zst <1826493691@qq.com>
2019-10-25 11:57:14 +0800

    [feat]添加新的聊天记录功能

--- 6/chatRoom/app.js
+++ 6/chatRoom/app.js
@@ -1,52 +1,57 @@
 let chatArea = document.getElementsByClassName('chatArea')[0];
 let images = chatArea.getElementsByTagName('img');
-let nameInput = chatArea.getElementsByTagName('ul')[0];
+let nameInput = chatArea.getElementsByTagName('input')[0];
 let textarea = chatArea.getElementsByTagName('textarea')[0];
 let sharingBtn = chatArea.getElementsByTagName('button')[0];
 
 let chatUl = document.getElementsByClassName('chatRecordList')[0].getElementsByTagName('ul')[0];
-let currentImageSrc="";
+let currentImageSrc="http://www.fgm.cc/learn/lesson6/img/face1.gif";
 
-// sharingBtn.onclick = function () {
-//     let newElement = createNode(currentImageSrc,getChatMessage(nameInput, textarea),getCurrentTime());
-//     let targetElement = chatUl.firstElementChild;
-//     chatUl.insertBefore(newElement, targetElement);
-// }
-// function createNode(image,message,time){
-//     // 判断是否存在message
-//     if(!message){
-//         return;
-//     }
-//     let li=document.createElement('li');
-//     li.innerHTML=`<img src=${image}>
-//     <p class="chatContent">${message}<p/>
-//     <span>${time}</span>`;
-// return li;   
-// }
-// function getCurrentTime(){
-//     let date = new Date();
-//     let month=date.getMonth()+1;
-//     let day=date.getDay();
-//     let hour=date.getHours();
-//     let minute=date.getMinutes();
-//     return month+"月"+day+"日"+" "+hour+":"+minute;
-// };
+/* 目前欠缺功能：
+头像点击后，维持红色边框;
+还能输入多少个字的提示
+插入新li的动画设置
+li的删除功能
+*/ 
+sharingBtn.onclick = function () {
+    let newElement = createNode(currentImageSrc,getChatMessage(nameInput, textarea),getCurrentTime());
+    let targetElement = chatUl.firstElementChild;
+    chatUl.insertBefore(newElement, targetElement);
+}
+function createNode(image,message,time){
+    // 判断是否存在message
+    if(!message){
+        return;
+    }
+    let li=document.createElement('li');
+    li.innerHTML="<img src="+image+"><p class='chatContent'>"+message+"</p><span>"+time+"</span>";
+return li;   
+}
+function getCurrentTime(){
+    let date = new Date();
+    let month=date.getMonth()+1;
+    let day=date.getDate();
+    let hour=date.getHours();
+    let minute=date.getMinutes();
+    return month+"月"+day+"日"+" "+hour+":"+minute;
+};
 
-// function getCurrentImage(){
-//     for(let i=0;i<images.length;i++){
+function getCurrentImage(){
+    for(let i=0;i<images.length;i++){
 
-//         images[i].onclick=function(){
-//             currentImageSrc=this.src;
-//         }
+        images[i].onclick=function(){
+            currentImageSrc=this.src;
+            console.log(currentImageSrc);
+        }
 
-//     }
-// }
-// getCurrentImage();
+    }
+}
+getCurrentImage();
 
 
 function getChatMessage(nameElement, contentElement) {
-    let nameMessage = nameElement.innerHTML;
-    let contentMessage = contentElement.innerHTML;
+    let nameMessage = nameElement.value;
+    let contentMessage = contentElement.value;
     if (nameMessage.length == 0) {
         alert('请输入姓名');
         return false;
