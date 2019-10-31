11a579a9c43448a3512b36246b050c70c807208c
zst <1826493691@qq.com>
2019-10-31 15:54:53 +0800

    [feat]添加遮罩层

--- 9/puzzle/app.js
+++ 9/puzzle/app.js
@@ -1,6 +1,7 @@
 let photos = document.getElementById('photos');
 let options = photos.getElementsByTagName('img');
 let puzzleBox = document.getElementById('puzzle');
+let startButton = document.getElementsByTagName('button');
 let imgPath = 0;
 // 设置拼图下标
 let imageData = [];
@@ -8,21 +9,21 @@ for (let i = 0; i < 15; i++) {
     imageData.push(i + 1);
 }
 
-// 初始化界面
+// 初始化缩略图
 for (let i = 0; i < options.length; i++) {
     options[i].onclick = function () {
 
         for (let j = 0; j < options.length; j++) {
             options[j].className = "";
         }
-
         this.className = "selected";
         imgPath = i;
-        initVersion();
+        startButton.innerHTML = "开始游戏";
     }
 }
 
-function initVersion() {
+// 添加拼图
+function addPuzzle() {
     puzzleBox.innerHTML = "";
 
     let fragment = document.createDocumentFragment();
@@ -35,6 +36,34 @@ function initVersion() {
     }
     // 事实上是添加了fragment的所有子元素
     puzzleBox.appendChild(fragment);
-    oBox.style.background = "url(img/girl" + imgPath + "/bg.png)  no-repeat";
+    puzzleBox.style.background = "url(http://www.fgm.cc/learn/lesson9/img/girl" + imgPath + "/bg.png)  no-repeat";
 }
-initVersion(0);
+
+// 遮罩
+function createMask() {
+    let mask = document.createElement('div');
+    mask.id = "mask";
+    puzzleBox.appendChild(mask);
+}
+
+function showMask() {
+    let mask = puzzleBox.getElementById('mask');
+    mask.style.display = "block";
+}
+
+function hideMask() {
+    let mask = puzzleBox.getElementById('mask');
+    mask.style.display = "none";
+}
+
+function playGame(isReady) {
+    if (isReady) {
+        imageData.sort(() => {
+            return Math.random() > 0.5 ? 1 : -1
+        });
+    }
+    addPuzzle();
+    createMask();
+    console.log(imageData);
+}
+playGame(false);
